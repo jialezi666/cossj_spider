2a4b19ea27a0c03fdd84fda49494ef5a2b3a2106
jialezi <592015984b@gmail.com>
2021-03-30 14:08:16 +0800

    3线程不炸

--- cossj.com.py
+++ cossj.com.py
@@ -1,4 +1,5 @@
 #-*- coding:utf-8 -*-
+import traceback
 from bs4 import BeautifulSoup
 import threading, time, requests, os, urllib3
 import json
@@ -6,32 +7,38 @@ import sys
 import urllib.request
 requests.packages.urllib3.disable_warnings()
 
-all_url = ['https://cos.cossj.com/Cosplay1.html','https://cos.cossj.com/Cosplay2.html','https://cos.cossj.com/Cosplay3.html']
-
 headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/65.0.3325.181 Safari/537.36',
     } 
 
-class StoppableThread(threading.Thread):
-    """封装stop和stoped的thread """
-
-    def __init__(self,  *args, **kwargs):
-        super(StoppableThread, self).__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
-
+class myThread (threading.Thread):   #继承父类threading.Thread
+    def __init__(self, url, dir, filename):
+        threading.Thread.__init__(self)
+        self.threadID = filename
+        self.url = url
+        self.dir = dir
+        self.filename=filename
+    def run(self):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数
+        download_pic(self.url,self.dir,self.filename)
+def download_pic(url,dir,filename):
+    req=requests.get(url=url,headers=headers)
+    file = dir+filename
+    if req.status_code==200:
+        with open(str(file),'wb') as f:
+            f.write(req.content)
 def mkdir(path):
     folder = os.path.exists(path)
     if not folder:
         d = os.makedirs(path)
     return path
-
+	
+def get_urls(num):
+        page_url = all_url[num-1]
+        print(page_url)
+        soup = creat_soup(page_url)
+        article_url=[a['href'] for a in soup.find_all('a', href=True)]
+        return article_url			
 def creat_soup( url):
     '''
     该函数返回一个url的soup对象
@@ -45,71 +52,42 @@ def creat_soup( url):
         print(e) 
     response.encoding = 'UTF-8' 
     return BeautifulSoup(response.text, 'html.parser')
-
-def get_urls(num):
-    #for page_url in all_url:
-        page_url = all_url[num-1]
-        print(page_url)
-        soup = creat_soup(page_url)
-        #get=requests.get(url=page_url,headers=headers)
-        #article_url=soup.find_all('a').get('href')
-        article_url=[a['href'] for a in soup.find_all('a', href=True)]
-        return article_url
-
-
-def get_single_imgs(url):
-    soup = creat_soup(url)
-    p = soup.findAll('li')
-    title = soup.find('title').text
-    page = {'title':title, 'imgs':[]}
-    for i in p:
-        page['imgs'].append(i['data-src'].replace("https://images.weserv.nl/?url=https://pic.24cos.com", "https://pic.lovecos.net"))
-    return page
-
-
-semaphore = threading.Semaphore(3)
-def saveImages(num, title, imgs):
-    imgPath = os.path.abspath('./')+'/cos'+num+'/'+title+'/'
-#    t = os.listdir(imgPath)
-    if os.path.exists(imgPath) and (len(os.listdir(imgPath)) == len(imgs) ):
-        print('Passed：'+title+'已经下载！')
-        return False
-    folder = mkdir(imgPath)
-    count = 0
-    print('开始下载:'+title+',共计图片:'+str(len(imgs)) +'张')
-    num = 5
-    with semaphore:
-        while num:
-            for index, img in enumerate(imgs):
-                try:
-                    html = requests.get(img, headers=headers, timeout=30)
-                    filename = folder+'/'+str(index+1)+'.jpg'
-                    with open(filename, 'wb') as handle:
-                        handle.write(html.content)
-                    count +=1
-                except Exception as e:
-                    print(e)
-                    continue
-            print('Finisded! '+title+'结束,共计下载'+str(count)+'张图片')
-
-def downTask():
+		
+try:
+    all_url = ['https://cos.cossj.com/Cosplay1.html','https://cos.cossj.com/Cosplay2.html','https://cos.cossj.com/Cosplay3.html']
     num = input("选择下载:\n1.动漫游戏\n2.古风制服\n3.日本和风\n")
     urls = get_urls(int(num)) 
     for url in urls:
-        thread_list = []
         url='https://cos.cossj.com/'+url
-        signle = get_single_imgs(url)
-        try:
-            th = StoppableThread(target=saveImages, args=(num,signle['title'],signle['imgs'])) 
-            th.start() 
-            thread_list.append(th)
-        except (KeyboardInterrupt, SystemExit):
-            th.stop()
-            sys.exit() 
-    for x in thread_list:
-        x.join()
-       
+        threads=[]
+        soup = creat_soup(url)
+        p = soup.findAll('li')
+        title = soup.find('title').text
+        page = {'title':title, 'imgs':[]}
+        for i in p:
+            page['imgs'].append(i['data-src'].replace("https://images.weserv.nl/?url=https://pic.24cos.com", "https://pic.lovecos.net"))
 
-
-if __name__=="__main__":
-    downTask()
+        imgPath = os.path.abspath('./')+'/cos'+num+'/'+title+'/'
+        imgs=page['imgs']
+        if os.path.exists(imgPath) and (len(os.listdir(imgPath)) == len(imgs) ):
+            print('Passed：'+title+'已经下载！')
+        else:
+            folder = mkdir(imgPath)
+            count = 0
+            print('开始下载:'+title+',共计图片:'+str(len(imgs)) +'张')
+            for index, img in enumerate(imgs):
+                filename = str(index+1)+'.jpg'
+                thread=myThread(img,imgPath,filename)
+                thread.start()
+                threads.append(thread)
+                count +=1
+                while True:
+                    if(len(threading.enumerate()) < 4):
+                        break
+            for t in threads:
+                t.join()
+            print('Finisded! '+title+'结束,共计下载'+str(count)+'张图片')
+except Exception as e:
+    print ('Exception: ', e)
+    print('程序错误，退出')
+    pass
